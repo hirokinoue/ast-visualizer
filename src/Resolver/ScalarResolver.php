c338d4da843ed761ac735e5a1696dae5b15a112c
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2023-11-30 23:35:26 +0900

    add resolver for scalar

--- src/Resolver/ScalarResolver.php
+++ src/Resolver/ScalarResolver.php
@@ -5,13 +5,24 @@ declare(strict_types=1);
 namespace Hirokinoue\AstVisualizer\Resolver;
 
 use PhpParser\Node;
+use PhpParser\Node\Scalar\DNumber;
+use PhpParser\Node\Scalar\Float_;
+use PhpParser\Node\Scalar\LNumber;
 use PhpParser\Node\Scalar\Int_;
+use PhpParser\Node\Scalar\InterpolatedString;
+use PhpParser\Node\Scalar\String_;
 
 class ScalarResolver implements Resolver
 {
     public function resolve(Node $node): string
     {
-        if ($node instanceof Int_) {
+        if ($node instanceof DNumber ||
+            $node instanceof Float_ ||
+            $node instanceof LNumber ||
+            $node instanceof Int_ ||
+            $node instanceof InterpolatedString ||
+            $node instanceof String_
+        ) {
             return (string)$node->value;
         }
         return '';
